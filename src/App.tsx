55cdd88bba082e3a48dce1dcb7d936aef9c3ed5a
sanzidakhatunbd <mostsanzidakhatunbd@gmail.com>
2026-09-12 20:32:18 +0600

    chore: initialize React project

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import Banner from "./components/Banner"
+import Nav from "./components/Nav"
+
+
+function App() {
+ 
+
+  return (
+    <>
+      <Nav />
+      <Banner />
+    </>
+  )
+}
+
+export default App
