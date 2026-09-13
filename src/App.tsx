c5b79f63c6061497d70c7f7bbb628ae4ca0d0daa
sanzidakhatunbd <mostsanzidakhatunbd@gmail.com>
2026-09-13 11:50:01 +0600

    Add JSON data and fetch functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
+import { Suspense } from "react";
 import Banner from "./components/Banner"
 import Nav from "./components/Nav"
+import Technologies from "./components/technologies/Technologies";
+import type { Itechnology } from "./types/techType";
 
-
+const technologiesFetch = async():Promise<Itechnology[]>=>{
+  const res = await fetch('/data.json');
+  const data = await res.json();
+  return data;
+}
 function App() {
- 
-
+  const technologiesPromise = technologiesFetch();
   return (
     <>
       <Nav />
       <Banner />
+      <Suspense fallback={<h2>Loading ... ...</h2>}>
+      <Technologies  technologiesPromise={technologiesPromise} />
+      </Suspense>
     </>
   )
 }
